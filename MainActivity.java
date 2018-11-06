6d114db143ae37a5e1b2867e257b4a6677bd2666
Michael Jia <loyaljadey@gmail.com>
2018-11-06 09:39:58 -0800

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.localiztionprgm;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        wireWidgets();
+    }
+
+    private void wireWidgets() {
+        textView = findViewById(R.id.textView);
+    }
+}
